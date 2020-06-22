fe573f0f3626194e7f9104b479118f745a4e7593
Barry Balasingham <bbalasingham@gmail.com>
2020-06-21 20:19:14 -0700

    [create] Create Main

---
+++ src/com/company/Stack.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Stack {
+    /**
+     * Default Constructor
+     */
+    public Stack()
+    {
+
+    }
+
+    /**
+     * Pushes an integer to the stack
+     * @param x - item to be pushed into stack
+     */
+    public void Push(int x)
+    {
+
+    }
+
+}
