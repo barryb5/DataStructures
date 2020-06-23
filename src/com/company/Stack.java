a021473c0592f38480156d77a39709cb8b95fe81
Barry Balasingham <bbalasingham@gmail.com>
2020-06-22 22:14:45 -0700

    [functions] added basic functions but they do not work

--- src/com/company/Stack.java
+++ src/com/company/Stack.java
@@ -1,21 +1,42 @@
 package com.company;
 
+import java.util.*;
+
+
 public class Stack {
     /**
      * Default Constructor
+     * @param size - size of the stack
      */
-    public Stack()
+    public Stack(int size)
     {
-
+        Stack stack = new Stack(size);
     }
 
     /**
      * Pushes an integer to the stack
      * @param x - item to be pushed into stack
      */
-    public void Push(int x)
+    public void Push(Stack stack, int x)
     {
-
+        stack.push(x);
     }
 
+    public int Pop(Stack stack)
+    {
+        int popped = stack.pop();
+        return popped;
+    }
+
+    public int Peek(Stack stack)
+    {
+        int peeked = stack.peek();
+        return peeked;
+    }
+
+    public boolean IsEmpty(Stack stack)
+    {
+        boolean empty = stack.empty();
+        return empty;
+    }
 }
